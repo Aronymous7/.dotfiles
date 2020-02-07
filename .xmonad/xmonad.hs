efe81a893629925ce41179c67d21b0046276ddf1
aaron <aaron.beigelbeck@yahoo.de>
2020-02-07 16:40:49 +0100

    some changes

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -13,26 +13,22 @@ import qualified XMonad.StackSet as W
     -- Utilities
 import XMonad.Util.Loggers
 import XMonad.Util.EZConfig (additionalKeysP, additionalMouseBindings)
-import XMonad.Util.NamedScratchpad
-import XMonad.Util.Run (safeSpawn, unsafeSpawn, runInTerm, spawnPipe)
+import XMonad.Util.Run (runInTerm, spawnPipe)
 import XMonad.Util.SpawnOnce
 
     -- Hooks
 import XMonad.Hooks.DynamicLog (dynamicLogWithPP, defaultPP, wrap, pad, xmobarPP, xmobarColor, shorten, PP(..))
-import XMonad.Hooks.ManageDocks (avoidStruts, docksStartupHook, manageDocks, ToggleStruts(..))
+import XMonad.Hooks.ManageDocks (avoidStruts, docksStartupHook, manageDocks)
 import XMonad.Hooks.ManageHelpers (isFullscreen, isDialog,  doFullFloat, doCenterFloat)
 import XMonad.Hooks.Place (placeHook, withGaps, smart)
 import XMonad.Hooks.SetWMName
-import XMonad.Hooks.EwmhDesktops   -- required for xcomposite in obs to work
 
     -- Actions
-import XMonad.Actions.Minimize (minimizeWindow)
+import XMonad.Actions.Promote
 import XMonad.Actions.RotSlaves (rotSlavesDown, rotAllDown)
-import XMonad.Actions.CopyWindow (kill1, copyToAll, killAllOtherCopies, runOrCopy)
+import XMonad.Actions.CopyWindow (kill1)
 import XMonad.Actions.WindowGo (runOrRaise, raiseMaybe)
-import XMonad.Actions.WithAll (sinkAll, killAll)
 import XMonad.Actions.CycleWS (moveTo, shiftTo, WSType(..), shiftNextScreen, shiftPrevScreen)
-import XMonad.Actions.DynamicWorkspaces (addWorkspacePrompt, removeEmptyWorkspace)
 import XMonad.Actions.MouseResize
 import qualified XMonad.Actions.ConstrainedResize as Sqr
 
@@ -42,20 +38,14 @@ import XMonad.Layout.Renamed (renamed, Rename(CutWordsLeft, Replace))
 import XMonad.Layout.WorkspaceDir
 import XMonad.Layout.Spacing (spacing)
 import XMonad.Layout.NoBorders
-import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
 import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
-import XMonad.Layout.Reflect (reflectVert, reflectHoriz, REFLECTX(..), REFLECTY(..))
 import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), Toggle(..), (??))
 import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
 import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
 
     -- Layouts
 import XMonad.Layout.GridVariants (Grid(Grid))
-import XMonad.Layout.SimplestFloat
-import XMonad.Layout.OneBig
-import XMonad.Layout.ThreeColumns
 import XMonad.Layout.ResizableTile
-import XMonad.Layout.ZoomRow (zoomRow, zoomIn, zoomOut, zoomReset, ZoomMessage(ZoomFullToggle))
 import XMonad.Layout.IM (withIM, Property(Role))
 
     -- Prompts
@@ -68,7 +58,7 @@ myFont				= "xft:DejaVu Sans:pixelsize=12"
 myModMask			= mod4Mask		-- Sets modkey to super/windows key
 myTerminal			= "urxvt"		-- Sets default terminal
 myTextEditor		= "vim"			-- Sets default text editor
-myBorderWidth		= 2				-- Sets border width for windows
+myBorderWidth		= 1				-- Sets border width for windows
 myFocusFollowsMouse	= False
 windowCount			= gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
 
@@ -76,7 +66,7 @@ main = do
     -- Launching xmobar.
     xmproc0 <- spawnPipe "xmobar"
     -- the xmonad, ya know...what the WM is named after!
-    xmonad $ ewmh desktopConfig
+    xmonad $ desktopConfig
         { manageHook = ( isFullscreen --> doFullFloat ) <+> manageHook desktopConfig <+> manageDocks
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = \x -> hPutStrLn xmproc0 x
@@ -94,7 +84,6 @@ main = do
         , terminal				= myTerminal
         , startupHook			= myStartupHook
         , layoutHook			= myLayoutHook
-        , workspaces			= myWorkspaces
         , borderWidth			= myBorderWidth
 		, focusFollowsMouse		= myFocusFollowsMouse
         , normalBorderColor		= "#292d3e"
@@ -107,15 +96,13 @@ main = do
 myStartupHook = do
           spawnOnce "nitrogen --restore &"
           setWMName "LG3D"
-          --spawnOnce "exec /usr/bin/trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 15 --transparent true --alpha 0 --tint 0x292d3e --height 19 &"
 
 ------------------------------------------------------------------------
 ---KEYBINDINGS
 ------------------------------------------------------------------------
 myKeys =
     --- Xmonad
-        [ ("M-C-r", spawn "xmonad --recompile")      -- Recompiles xmonad
-        , ("M-S-r", spawn "xmonad --restart")        -- Restarts xmonad
+        [ ("M-S-r", spawn "xmonad --restart")        -- Restarts xmonad
         , ("M-S-e", io exitSuccess)                  -- Quits xmonad
 
     --- Windows
@@ -131,70 +118,46 @@ myKeys =
         , ("M-S-i", windows W.swapMaster)            -- Swap the focused window and the master window
         , ("M-S-j", windows W.swapDown)              -- Swap the focused window with the next window
         , ("M-S-k", windows W.swapUp)                -- Swap the focused window with the prev window
-        , ("M1-S-<Tab>", rotSlavesDown)              -- Rotate all windows except master and keep focus in place
-        , ("M1-C-<Tab>", rotAllDown)                 -- Rotate all the windows in the current stack
-
-        , ("M-C-M1-<Up>", sendMessage Arrange)
-        , ("M-C-M1-<Down>", sendMessage DeArrange)
+        , ("M-S-<Tab>", rotSlavesDown)               -- Rotate all windows except master and keep focus in place
+        , ("M-C-<Tab>", rotAllDown)                  -- Rotate all the windows in the current stack
+        , ("M-<Backspace>", promote)                 -- Moves focused window to master, all others maintain order
 
     --- Layouts
         , ("M-<Tab>", sendMessage NextLayout)				-- Switch to next layout
-        , ("M-S-<Tab>", sendMessage ToggleStruts)           -- Toggles struts
-        , ("M-S-n", sendMessage $ Toggle NOBORDERS)         -- Toggles noborder
-		, ("M-S-f", sendMessage (T.Toggle "float"))
 
         , ("M-C-h", sendMessage Shrink)
         , ("M-C-l", sendMessage Expand)
         , ("M-C-j", sendMessage MirrorShrink)
         , ("M-C-k", sendMessage MirrorExpand)
-        , ("M-S-;", sendMessage zoomReset)
-        , ("M-;", sendMessage ZoomFullToggle)
 
     --- Workspaces
-        , ("M-l", moveTo Next nonNSP)							-- Go to next workspace
-        , ("M-h", moveTo Prev nonNSP)							-- Go to previous workspace
+        , ("M-l", moveTo Next nonEmptyNonNSP)					-- Go to next non-empty workspace
+        , ("M-h", moveTo Prev nonEmptyNonNSP)					-- Go to previous non-empty workspace
+		, ("M-d", moveTo Next emptyNonNSP)
         , ("M-S-l", shiftTo Next nonNSP >> moveTo Next nonNSP)	-- Shifts focused window to next workspace
         , ("M-S-h", shiftTo Prev nonNSP >> moveTo Prev nonNSP)	-- Shifts focused window to previous workspace
-
-    --- Open Terminal
-        , ("M-<Return>", spawn myTerminal)
+		, ("M-S-d", shiftTo Next emptyNonNSP)
 
     --- Run Programs
+        , ("M-<Return>", spawn myTerminal)
         , ("M-s", spawn "dmenu_run")
         , ("M-b", spawn "firefox")
         , ("M-n", spawn "gsimplecal")
         , ("M-m", spawn "thunderbird")
         ] where nonNSP          = WSIs (return (\ws -> W.tag ws /= "nsp"))
                 nonEmptyNonNSP  = WSIs (return (\ws -> isJust (W.stack ws) && W.tag ws /= "nsp"))
-
-------------------------------------------------------------------------
----WORKSPACES
-------------------------------------------------------------------------
-
-xmobarEscape = concatMap doubleLts
-  where
-        doubleLts '<' = "<<"
-        doubleLts x   = [x]
-
-myWorkspaces :: [String]
-myWorkspaces = clickable . (map xmobarEscape)
-               $ ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  where
-        clickable l = [ "<action=xdotool key super+" ++ show (n) ++ ">" ++ ws ++ "</action>" |
-                      (i,ws) <- zip [1..9] l,
-                      let n = i ]
+                emptyNonNSP		= WSIs (return (\ws -> not (isJust (W.stack ws)) && W.tag ws /= "nsp"))
 
 ------------------------------------------------------------------------
 ---LAYOUTS
 ------------------------------------------------------------------------
 
-myLayoutHook = avoidStruts $ mouseResize $ windowArrange $ T.toggleLayouts floats $
+myLayoutHook = avoidStruts $ mouseResize $ windowArrange $
                mkToggle (NBFULL ?? NOBORDERS ?? EOT) $ myDefaultLayout
              where
-                 myDefaultLayout = tall ||| grid ||| noBorders monocle ||| floats
+                 myDefaultLayout = tall ||| grid ||| noBorders monocle
 
 
-tall	= renamed [Replace "tall"]     $ limitWindows 12 $ spacing 6 $ ResizableTall 1 (3/100) (1/2) []
-grid	= renamed [Replace "grid"]     $ limitWindows 12 $ spacing 6 $ mkToggle (single MIRROR) $ Grid (16/10)
-monocle	= renamed [Replace "monocle"]  $ limitWindows 20 $ Full
-floats	= renamed [Replace "floats"]   $ limitWindows 20 $ simplestFloat
+tall	= renamed [Replace "tall"]     $ spacing 6 $ ResizableTall 1 (3/100) (1/2) []
+grid	= renamed [Replace "grid"]     $ spacing 6 $ mkToggle (single MIRROR) $ Grid (16/10)
+monocle	= renamed [Replace "monocle"]  $ Full
