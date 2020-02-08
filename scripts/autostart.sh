aed405d572e4efef31fa0eeff861ab4e6081f2f6
aaron <aaron.beigelbeck@yahoo.de>
2020-02-09 00:21:33 +0100

    some changes

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -5,3 +5,4 @@ nitrogen --restore &
 [[ -z $(pgrep unclutter) ]] && exec unclutter &
 [[ -z $(pgrep devmon) ]] && exec devmon &
 [[ -z $(pgrep dunst) ]] && exec dunst &
+[[ -z $(pgrep sxhkd) ]] && exec sxhkd &
