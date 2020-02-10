c4bbb35cbfd77213a52b876e82c85abee9ffb183
aaron <aaron.beigelbeck@yahoo.de>
2020-02-10 21:35:40 +0100

    some changes

--- scripts/get_master_volume.sh
+++ scripts/get_master_volume.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "vol $(amixer -M get Master | grep -o "[0-9]*%\|\[on\]\|\[off\]" | sed 2q | tr '\n' ' ')"
+echo "vol $(amixer -M get Master | grep -o "[0-9]*%\|\[on\]\|\[off\]" | tr '\n' ' ' | sed "s/ [0-9].*$//")"
