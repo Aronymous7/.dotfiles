bc66a7e045c4e590665dee5191d8aa2ff0d104ba
Aaron Beigelbeck <aaron.beigelbeck@yahoo.de>
2020-03-12 21:54:08 +0100

    some changes

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Run these programs at system startup. This script gets executed in ~/.xinitrc
+
 nitrogen --restore &
 [[ -z $(pgrep xss-lock) ]] && exec xss-lock -- i3lock -n -c 000000 &
 [[ -z $(pgrep syncthing) ]] && exec syncthing -no-browser &
