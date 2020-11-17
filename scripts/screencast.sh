0db7bfcba63914b764bd73886b6aeb5f7042a023
Aaron Beigelbeck <aaron.beigelbeck@yahoo.de>
2020-11-17 11:22:26 +0100

    some changes

--- scripts/screencast.sh
+++ scripts/screencast.sh
@@ -6,4 +6,19 @@ ffmpeg \
 -i :0.0 \
 -f alsa \
 -i default \
-out.mkv
+$1
+
+# Useful tips for video editing:
+#
+# Concatenating:
+# ffmpeg -f concat -i list.txt -c copy out.mkv
+# With list.txt looking something like:
+# file 'out1.mkv'
+# ...
+#
+# Trimming:
+# ffmpeg -i out.mkv -ss 00:00:05 -to 00:00:10 out.mkv
+# with relative time:
+# ffmpeg -i out.mkv -ss 00:00:05 -t 00:00:05 out.mkv
+# without re-encoding:
+# ffmpeg -i out.mkv -ss 00:00:05 -to 00:00:10 -c copy -avoid_negative_ts make_zero out.mkv
